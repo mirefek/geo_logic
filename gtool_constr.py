34fd8d6d72ea232771ea5f581623493f97317cd5
mirefek <mirek@olsak.net>
2020-02-25 15:24:52 +0100

    less accurate center_of by dragging

--- gtool_constr.py
+++ gtool_constr.py
@@ -223,7 +223,12 @@ class ComboPoint(GToolConstr):
                 return
 
         cl2,cln2 = self.select_cl(coor)
-        if cl2 is None or cl2 == cl1: return
+        if cl2 is None and isinstance(cln1, Circle):
+            if np.linalg.norm(coor - cln1.c) < cln1.r:
+                self.hl_propose(Point(cln1.c))
+                self.confirm = self.run_tool, "center_of", cl1
+            return
+        if cl2 == cl1: return
 
         intersections = self.intersect(cln1, cln2)
         if intersections is None: return
